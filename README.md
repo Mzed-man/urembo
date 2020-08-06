ead964abc6fe39a6898600924ec5ebf41c8cd3c3
Pacifique M. Mirindi <pacifiquem.mirindi@MacBook-Pro-de-Pacifique.local>
2020-08-06 13:22:23 +0000

    the beginning of an adventure

---
+++ README.md
@@ -0,0 +1,18 @@
+# urembo
+
+> scss framework for inside work for MzedSoft and affiliated partners and/or acquisitions
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+```
+
+For detailed explanation on how things work, consult the [docs for vue-loader](http://vuejs.github.io/vue-loader).
---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <div class="nav big center secondary">
+      <div class="logo">
+        <img src="./assets/logo.png">
+      </div>
+      <div class="menu">
+        <a href="http://" class="menu-item">Navigation 1</a>
+        <a href="http://" class="menu-item">Navigation 2</a>
+        <a href="http://" class="menu-item">Navigation 3</a>
+        <a href="http://" class="menu-item">Navigation 4</a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style lang="scss">
+  @import './styles/styles.scss'
+</style>
